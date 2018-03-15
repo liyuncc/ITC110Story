4f54f658f928f4824226cac15ee56b9fe39d3a56
liyuncc <liyuncecil@gmail.com>
2018-03-14 21:01:15 -0700

    updated text

--- app.py
+++ app.py
@@ -19,22 +19,21 @@ def madstory():
         'noun4' : request.form['noun4'],
         'verb1' : request.form['verb1'],
         'verb2' : request.form['verb2'],
-        'verb3' : request.form['verb3'],
         'verb4' : request.form['verb4'],
         'verb5' : request.form['verb5'],
         'verb6' : request.form['verb6'],
         'common_noun1' : request.form['common_noun1'],
         'common_noun2' : request.form['common_noun2'],
         'adverb1' : request.form['adverb1'],
-        
+
     }
     return render_template('story.html', madwords = madwords)
-    
 
-    
+
+
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 8080))
     host = os.getenv('IP', '0.0.0.0')
     app.run(port = port, host = host)
-    
+
     # local porting info etc
